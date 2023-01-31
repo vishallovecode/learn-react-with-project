25c1057c6f430432406bcb690ff72fd96a0ec89e
Vishal Sharma <vishalsharma@ip-192-168-1-2.ap-south-1.compute.internal>
2023-01-31 22:12:55 +0530

    update lifecycle

--- src/Home/LifeCycle.jsx
+++ src/Home/LifeCycle.jsx
@@ -1,80 +1,105 @@
 import { Component } from "react";
 
-
 class LifeCycle extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loader: false,
+      products: [],
+      counter: 0,
+    };
+  }
 
-    constructor(props){
-        super(props);
-        this.state = {
-            loader: false,
-            products: []
-        }
-    }
+  fetchProductData = async () => {
+    const state = { ...this.state }; // state reference now it the value copy
+    try {
+      state.loader = true;
+      this.setState(state);
 
-fetchProductData = async()=> {
-    const state = {...this.state};  // state reference now it the value copy
-    try  {
-        state.loader = true;
-        console.log('state=>' , state);
+      const res = await fetch("https://dummyjson.com/products");
+      const result = await res.json();
+      state.products = result?.products;
+      state.loader = false;
+      this.setState(state); // this.setState behave like async
+    } catch (error) {
+      state.loader = false;
+      this.setState(state);
+      alert("Something went wrong!!! Please try Again!!");
+    }
+  };
+  componentWillMount() {
+    // deprceated
+  }
+  componentDidMount() {
+    this.fetchProductData();
+  }
+  // optimize perspective everutime we donot want to render the ui
 
-       this.setState(state)
-       console.log('state after loader set=>' , this.state);
-        const res = await fetch("https://dummyjson.com/products");
-        const result = await res.json();
-        // console.log(result)
-        state.products = result?.products;
-        state.loader = false 
-        this.setState(state) ; // this.setState behave like async
+  manipulateProducst = (products) => {
+    const updatedProducts = products.map((pr) => {
+      console.log(pr)
+      return {
+        ...pr,
+        price: "RS." + pr.price,
+      };
+    });
+    console.log(updatedProducts)
+    const updatedState = { ...this.state };
+    updatedState.products = updatedProducts;
+    this.setState(updatedState);
+  };
 
-    } catch(error) {
-     state.loader = false;
-     this.setState(state);
-     alert("Something went wrong!!! Please try Again!!")
+  componentDidUpdate(prevProps, prevState) {
+    console.log("prevState=>>", prevState, "current state=>", this.state);
+    if (prevState.counter != this.state.counter) {
+      this.manipulateProducst(this.state.products);
     }
-}
-    componentWillMount() {
-   
-    }
-    componentDidMount() {
-        this.fetchProductData();
-    }
-    render() {
-        return (
-            <div>
-    
-            {this.state.loader ? (
-              <div>
-                <img src="/Loading_icon.gif" />
-              </div>
-            ) : (
-              this.state.products.map((pr) => {
-                // console.log(pr); //
-                return (
-                  <div>
-                    <h6>{pr.title}</h6>
-                    <div>
-                      <span>Brand::</span> <span>{pr.brand}</span>
-                    </div>
-                    <div>
-                      <span>Price::</span> <span>{pr.price}</span>
-                    </div>
-                  </div>
-                );
-              })
-            )}
+  }
+
+  updateCounter = () => {
+    const updatedState = { ...this.state };
+    updatedState.counter = updatedState.counter + 1;
+    this.setState(updatedState);
+  };
+  componentWillUnmount() {
+    // this will call once the component unmount from the dom => remove from the dom tree 
+    // mainly this used a for unsubscribe the thing and cleaning perspective
+  }
+  // anything get updaTED
+  render() {
+    console.log("render", this.state);
+    return (
+      <div>
+        <button onClick={this.updateCounter}>Update Counter</button>
+        {this.state.loader ? (
+          <div>
+            <img src="/Loading_icon.gif" />
           </div>
-        )
-    }
-
+        ) : (
+          this.state.products.map((pr) => {
+            // console.log(pr); //
+            return (
+              <div>
+                <h6>{pr.title}</h6>
+                <div>
+                  <span>Brand::</span> <span>{pr.brand}</span>
+                </div>
+                <div>
+                  <span>Price::</span> <span>{pr.price}</span>
+                </div>
+              </div>
+            );
+          })
+        )}
+      </div>
+    );
+  }
 }
-
 
 export default LifeCycle;
 
-
 // Initialiozation  => constructor();
 //Mounting =>  componentWillUnmount called jsut before the component mounts on the dom or render function called
-        //    => componenntDidMount  => this method called after the component mount on the dom
+//    => componenntDidMount  => this method called after the component mount on the dom
 
-
-        // api => mounting  , intialize , unmonting , update
+// api => mounting  , intialize , unmonting , update
