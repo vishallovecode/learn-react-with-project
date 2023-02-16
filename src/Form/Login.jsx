8474d223bd018f546eb8a8f1e8dbf3fe22a5cff8
Vishal Sharma <vishalsharma@Vishals-MacBook-Pro.local>
2023-02-16 22:21:36 +0530

    task form

--- src/Form/Login.jsx
+++ src/Form/Login.jsx
@@ -1,70 +1,76 @@
-import { useState } from "react"
+import { useState } from "react";
 
 const Login = () => {
+  const [loginData, setLoginData] = useState({
+    password: "",
+    userName: "",
+  });
 
-    const [loginData , setLoginData] = useState({
-        password: '',
-        userName: ''
-    })
+  // const  [password , setPassword] = useState('');
+  // const  [user_name , setUserName] = useState('');
 
-    // const  [password , setPassword] = useState('');
-    // const  [user_name , setUserName] = useState('');
+  //     const userNameHandler = (e) => {
+  //         setUserName(e.target.value)
+  // setLoginData({...loginData, 'userName': e.target.value})
+  //     }
 
-    
-    //     const userNameHandler = (e) => {
-    //         setUserName(e.target.value)
-    //     }
+  //     const passwordHandler = (e)=>  {
+  //         setPassword(e.target.value)
+  //     }
 
-    //     const passwordHandler = (e)=>  {
-    //         setPassword(e.target.value)
-    //     }
+  const onChangeHandler = (e) => {
+    setLoginData({ ...loginData, [e.target.name]: e.target.value });
+  };
 
+  const loginHandler = (data) => {
+    console.log("hey", data);
+    console.log( "Login SuccessFully", loginData.userName,  "   ", loginData.password );
+    alert("Hye");
+  };
 
-    const onChangeHandler = (e)=> {
-        setLoginData({...loginData, [e.target.name]: e.target.value})
-    }
+  const onChangeHandlerType = (e, type) => {
+    setLoginData({ ...loginData, [type]: e.target.value });
+  };
 
-        
-    const loginHandler = (data)=>{
-        console.log("hey" , data)
-        console.log("Login SuccessFully" , loginData.userName , "   " , loginData.password)
-        alert("Hye")
-    }
+  // Uncontrolled
+  // Controlled
+  return (
+    <div>
+      <form onSubmit={loginHandler}>
+        <input
+          placeholder="UserName"
+          required
+          onChange={(e) => onChangeHandlerType(e, "userName")}
+          value={loginData.userName}
+        />
+        <input
+          placeholder="Password"
+          required
+          onChange={(e) => onChangeHandlerType(e, "password")}
+          value={loginData.password}
+        />
+        <select>
+          <option>English</option>
+          <option>Hindi</option>
+          <option>Marathi</option>
+        </select>
 
-
-    const onChangeHandlerType = (e, type)=> {
-        setLoginData({...loginData, [type]: e.target.value})
-    }
-
-    // Uncontrolled
-    // Controlled
-    return  (
-        <div>
-            <form  onSubmit={loginHandler}>
-                <input placeholder="UserName"  required onChange={(e)=>onChangeHandlerType(e, 'userName')} value = {loginData.userName}/>
-                <input placeholder="Password"   required onChange={(e)=>onChangeHandlerType(e,'password')} value= {loginData.password}/>
-                {/* <input placeholder="UserName"  type = 'email'  name = "userName"  onChange={onChangeHandler} value = {loginData.userName}/>
+        {/* <input placeholder="UserName"  type = 'email'  name = "userName"  onChange={onChangeHandler} value = {loginData.userName}/>
                 <input placeholder="Password"   name = "password" required onChange={onChangeHandler} value= {loginData.password}/> */}
-                <button type= 'submit' value={"Login"}>Submit</button>
-            </form>
-        </div>
-    )
-}
+        <button type="submit" value={"Login"}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
 
-
 // if we want to give key as dynamic key in object put the variable inside the []
 
-let key = "College"
+let type = "";
 
-const data  = {
-    [key]: "LPU"
-}
-
-
-
-
-
-
-
+const data = {
+  [key]: "LPU",
+};
